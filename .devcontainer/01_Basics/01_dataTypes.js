e245a7278e2e364896f0a903f4296fb527624465
Abishek2012 <95891779+Abishek2012@users.noreply.github.com>
2025-01-22 11:44:38 +0000

    Data Types

---
+++ .devcontainer/01_Basics/01_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // we are using node engine/ node js , not browser or document object model
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+//  symbol => unique
+
+
+
+//object
+
+
+console.log(typeof Object);
