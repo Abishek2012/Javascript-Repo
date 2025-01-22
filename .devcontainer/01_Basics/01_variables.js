dea984632a8e74e28e8e961666b5a7e08a06c38a
Abishek2012 <95891779+Abishek2012@users.noreply.github.com>
2025-01-22 11:19:35 +0000

    variable

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail = "abishek@lallu.com"
+var accountPassword = "12345" 
+accountCity = "Jaipur"
+
+// accountID = 345678
+
+accountEmail = "pappu@gmail.com"
+accountPassword = "234511"
+accountCity = "Delhi"
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
+/* Should not use var due to block scope and functional scope*/
+
