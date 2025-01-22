2af056e3af61be70229d2d02a1b1d250b68b9ac6
Abishek2012 <95891779+Abishek2012@users.noreply.github.com>
2025-01-22 14:25:35 +0000

    TDZ2

--- .devcontainer/01_Basics/02_dataTypes.js
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -1,8 +1,18 @@
 let score = "33"
-console.log(typeof score);
+// console.log(typeof score);
 
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(typeof NaN);
+// console.log(typeof valueInNumber);
+// console.log(typeof NaN);
+
+let isLoggedIn = Number
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+console.log(typeof isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+
--- .devcontainer/01_Basics/TemporalDeadZone.js
+++ .devcontainer/01_Basics/TemporalDeadZone.js
@@ -1,24 +1,31 @@
-// let a =10
-// let a = 100
+// // let a =10
+// // let a = 100
 
-// This will throw a syntax error as duplication of let is not allowed and let and const lies in temporal dead zones which is the time from which the let & const are hoisted till the time it is initialized.
+// // This will throw a syntax error as duplication of let is not allowed and let and const lies in temporal dead zones which is the time from which the let & const are hoisted till the time it is initialized.
 
-let a;
+// let a;
 
-a = 24
+// a = 24
 
-console.log(a);
+// console.log(a);
 
-var b;
+// var b;
 
-b = 70
+// b = 70
 
-console.log(b);
-
-
-const c = 20;
-
-a = 100
-console.log(a);
+// console.log(b);
 
 
+// const c = 20;
+
+// a = 100
+// console.log(a);
+
+
+
+
+{
+//    Compound Statement
+}
+
+//  Block, Scope and Shadowing as well as Illegal Shadowing
