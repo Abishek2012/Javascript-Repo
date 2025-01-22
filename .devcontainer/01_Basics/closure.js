3c1ac638ed360499e5225b947f9eece0330c4e79
Abishek2012 <95891779+Abishek2012@users.noreply.github.com>
2025-01-22 15:41:25 +0000

    closure

---
+++ .devcontainer/01_Basics/closure.js
@@ -0,0 +1,21 @@
+function z(){
+    var b = 20
+    function x(){
+        var a = 7
+         function y(){
+        
+            console.log(a, b);
+            
+        }
+          y()
+    }
+        console.log (x())
+}
+z()
+
+
+
+
+// uses of closure       Module Design patterns, Currying, Function Like Once, Memoize, Maintaining state in Async world, setTimeouts, Iterators
+
+//  Functions along with its lexical scope bundled together forms a closure.
