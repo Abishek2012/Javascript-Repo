11c9aa71dd87a445e0d4895bc1cff6497d312f83
Abishek2012 <95891779+Abishek2012@users.noreply.github.com>
2025-01-26 10:01:44 +0000

    Destructure

---
+++ .devcontainer/01_Basics/destructuring.js
@@ -0,0 +1,21 @@
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseTeacher : "Abishek"
+}
+
+const {courseTeacher : teacher, coursename : name} = course
+
+console.log(teacher , name);
+
+
+
+
+// const navbar = () => {
+
+// }
+
+{
+    "userName" : "Abishek",
+    "price" : "200"
+}
