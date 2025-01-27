50f4bbf90b73aafc039bce2ca5263f6484f675d9
Abishek2012 <95891779+Abishek2012@users.noreply.github.com>
2025-01-27 07:20:54 +0530

    JS

--- .devcontainer/01_Basics/map.js
+++ .devcontainer/01_Basics/map.js
@@ -4,9 +4,9 @@ const myNums = [1,2,3,4,5,6,7,8]
 const newNums = myNums
 .map( (num) => num * 10)
 .map( (num) => num + 10)
-.filter( (num) => num >=20)
+.filter( (num) => num <=60)
 console.log(newNums);
-// console.log(myNums);
+//  console.log(myNums);
 
 
 
